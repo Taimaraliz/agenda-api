65983f7d7ae360ff092c428467a20310529e9c02
Taimara Liz <tailizadm@gmail.com>
2022-07-30 20:46:24 -0300

    Initial commit

---
+++ src/main/java/br/com/agenda/agendaapi/api/rest/ContatoController.java
@@ -0,0 +1,71 @@
+package br.com.agenda.agendaapi.api.rest;
+
+import br.com.agenda.agendaapi.model.entity.Contato;
+import br.com.agenda.agendaapi.model.repository.ContatoRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/contatos")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class ContatoController {
+
+    private final ContatoRepository repository;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Contato save ( @RequestBody Contato contato ){
+        return repository.save(contato);
+
+    }
+    @DeleteMapping( "{id}" )
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete( @PathVariable Integer id ){
+        repository.deleteById(id);
+    }
+
+    @GetMapping
+    public List<Contato> list(){
+        return repository.findAll();
+    }
+    @PatchMapping("{id}/favorito")
+    public void favorite( @PathVariable Integer id ){
+        Optional<Contato> contato = repository.findById(id);
+        contato.ifPresent( c -> {
+            boolean favorito = c.getFavorito() == Boolean.TRUE;
+            c.setFavorito(!favorito);
+            repository.save(c);
+        });
+
+    }
+
+    @PutMapping("{id}/foto")
+    public byte[] addFoto(@PathVariable Integer id,
+                          @RequestParam("foto")Part arquivo){
+        Optional<Contato> contato = repository.findById(id);
+        return contato.map(c ->{
+            try {
+                InputStream is = arquivo.getInputStream();
+                byte[] bytes = new byte[(int) arquivo.getSize()];
+
+                IOUtils.readFully(is, bytes);
+                c.setFoto(bytes);
+                repository.save(c);
+                is.close();
+                return bytes;
+            } catch (IOException e) {
+                return null;
+            }
+        }).orElseThrow(null);
+
+    }
+}
+
